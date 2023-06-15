09c6ec9c360566443d15ec3996c1cf6fc1674833
vishal <vislearn1983@gmail.com>
2023-06-16 00:00:00 +0530

    day9

---
+++ Day9/Q1/meth1.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    void find(vector<int> arr,int i,int n,vector<int> &ans,int &sum){
+        if(i>=n){ans.push_back(sum);return;}
+        // cout<<" i:"<<i<<" "<<sum<<" |";
+        int inc=sum+arr[i];
+        find(arr,i+1,n,ans,inc);
+        ans.push_back(sum);
+        find(arr,i+1,n,ans,sum);
+        
+    }
+    vector<int> subsetSums(vector<int> arr, int N)
+    {
+        // Write Your Code here
+        vector<int> ans;
+        int i=0;int sum=0;
+        find(arr,i,N,ans,sum);
+        // vector<int> out;
+        // for(auto val:ans){
+        //     out.push_back(val);
+        // }
+        return ans;
+    }
+};
