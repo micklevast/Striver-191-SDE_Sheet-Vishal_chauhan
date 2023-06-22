d706800c173dfa688a545bc6ca0903e7a1074920
vishal <vislearn1983@gmail.com>
2023-06-23 00:37:07 +0530

    Day 16

---
+++ Day14/Q8/meth2.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h> 
+using namespace std;
+class StockSpanner {
+public:
+    stack<pair<int, int>> st;
+    
+    StockSpanner() {
+        
+    }
+    
+    int next(int price) {
+        int span = 1;
+        
+        while (!st.empty() && price >= st.top().first) {
+            span += st.top().second;
+            st.pop();
+        }
+        
+        st.push({price, span});
+        
+        return span;
+    }
+};
