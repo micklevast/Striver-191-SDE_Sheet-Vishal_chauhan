a109dc1bb6bad395b27dd5c7551122229c43ec9b
vishal <vislearn1983@gmail.com>
2023-06-24 23:59:46 +0530

    Day19

--- Day16/Q2/meth1.cpp
+++ Day16/Q2/meth1.cpp
@@ -1,3 +1,6 @@
-#include<iostream>
-#include<bits/stdc++.h>
-using namespace std;
+#include <bits/stdc++.h> 
+bool findPattern(string p, string s)
+{
+    // Write your code here.
+    return s.find(p)!=string::npos?1:0;
+}
