0e882239db21e4641a4bc4fa80f1bc1c5d562b00
vishal <vislearn1983@gmail.com>
2023-07-01 23:59:37 +0530

    Day24

--- Day16/Q5/meth1.cpp
+++ Day16/Q5/meth1.cpp
@@ -1,3 +1,33 @@
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
+class Solution {
+public:
+    string cas(int n){
+        if(n==1)return "1";
+
+        string sub=cas(n-1);
+
+        char counting=sub[0];
+        int c=1;
+        string ans="";
+        for(int i=1;i<sub.length();i++){
+            if(sub[i]==counting){
+                c++;
+            }
+            else{
+                ans+=(to_string(c)+counting);
+                counting=sub[i];
+                c=1;
+            }
+        }
+        ans+=(to_string(c)+counting);
+        return ans;
+    }
+    string countAndSay(int n) {
+        if(n==1)return "1";
+
+        string ans=cas(n);
+        return ans;
+    }
+};
---
+++ Day24/Q2/meth1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include<bitset>/std.c++.h>
+using namespace std;
+class Solution
+{
+	public:
+	//Function to find the shortest distance of all the vertices
+    //from the source vertex S.
+    vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
+    {
+        // Code here
+        vector<int> dist(V,INT_MAX);
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        dist[S]=0;
+        pq.push({0,S});
+        while(!pq.empty()){
+            int nd=pq.top().second;
+            int wgh=pq.top().first;
+            pq.pop();
+            
+            for(auto vec:adj[nd]){
+                int val=vec[1];
+                int node=vec[0];
+                if(dist[nd]+val < dist[node]){
+                    dist[node]=dist[nd]+val;
+                    pq.push({dist[node],node});
+                }
+            }
+        }
+        return dist;
+    }
+};
